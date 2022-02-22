fe159093e2c7420641ede94bce4cf76b129afdce
gnganapath <gnganapathi@gmail.com>
2022-02-23 03:41:25 +0530

    useMemo hooks - memo heavy fun, make seperation of render

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import  './App.css';
 import { FunctionComponent } from './functioncomponent';  // type-3 => arrya export
 import  UseStateHookDemo from './hooks-components/useStateHook';   // type-1 =>typical 2 line export default
-import { UseEffectHook } from './hooks-components/useeffecthook';   // type-2 =>export without default 
+import { UseEffectHook } from './hooks-components/useEffectHook';   // type-2 =>export without default 
 import  UseRefHook from './hooks-components/useRefHook'; 
 import {UseContextUsages} from './hooks-components/useContextScenerioDemo/useContextUsages';
 import { UseContextHook } from './hooks-components/useContextHook/useContextHook';
+import { UseMemoHook } from './hooks-components/useMemoHook';
 
 function App() {
   return (
@@ -23,6 +24,8 @@ function App() {
         </>
 
         <UseRefHook />
+
+        <UseMemoHook />
       </div>
     </div>
   );
--- react-hooks/src/hooks-components/useeffecthook.js
+++ react-hooks/src/hooks-components/useeffecthook.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ClassComponent from './ClassComponent';
 import { UseEffectFunctionComponent } from './UseEffectFunctionComponent';
 
+
+/*
+// Type-1 compomnent mounted and updated - first time and Every method call/ variable update
+// Type-2 Call only once which is component mounted - Mostly for Fetch / Axios call 
+// Type-3 runs any time any dependency value changes
+*/
 export function UseEffectHook() {
     return (
         <div className="App" style={{border: '1px solid blue', margin: 5}}>
