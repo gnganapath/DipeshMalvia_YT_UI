6c48a7e0fa2f232f40a739e9c17a5a378c69200f
gnganapath <gnganapathi@gmail.com>
2026-02-22 18:59:29 +0530

    reefence link to components

---
+++ react-recall-feb2026/src/components/LazyComponent.tsx
@@ -0,0 +1,10 @@
+// const LazyLoginComponent = lazy(()=>import("./components/Login")); 
+
+
+  /*<Route path="/login" element={
+          <Suspense fallback="...loading...">
+            <LazyLoginComponent />
+          </Suspense>
+        } />*/
+
+        //refer at App.tsx
